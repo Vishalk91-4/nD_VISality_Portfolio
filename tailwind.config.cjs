a7dc175dd0d2a2ede212113bd009741bcb1be1a0
Vishal K <116670999+Vishalk91-4@users.noreply.github.com>
2023-05-06 13:37:29 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./index.html","./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
     extend: {
