6837db2bec5072e1bcf2598f169443526d4e2bda
Vishal K <116670999+Vishalk91-4@users.noreply.github.com>
2023-05-17 12:04:05 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,9 +15,9 @@ const ServiceCard = ({ index, title, icon }) => (
     >
       <div
         options={{
-          max: 75,
+          max: 45,
           scale: 1,
-          speed: 900,
+          speed: 450,
         }}
         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
